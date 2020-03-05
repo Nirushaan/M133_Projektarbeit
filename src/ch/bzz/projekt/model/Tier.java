8015e66987fff6d2ce2706c881ef2b58e05b202e
Nirushaan Sureshkumar <sureshkumarn@bzz.ch>
2020-03-05 16:01:04 +0100

    initial commit

---
+++ src/ch/bzz/projekt/model/Tier.java
@@ -0,0 +1,173 @@
+package ch.bzz.projekt.model;
+
+/**
+ * short description
+ * <p>
+ * Projektarbeit
+ *
+ * @author Nirushaan Sureshkumar
+ * @version 1.0
+ * @since 27.02.20
+ */
+public class Tier {
+    private String animal;
+    private int animalID;
+    private String name;
+    private int age;
+    private String gender;
+    private double height;
+    private String birthday;
+    private String food;
+
+    /**
+     * Gets the animal
+     *
+     * @return value of animal
+     */
+    public String getAnimal() {
+        return animal;
+    }
+
+    /**
+     * Sets the animal
+     *
+     * @param animal the value to set
+     */
+
+    public void setAnimal(String animal) {
+        this.animal = animal;
+    }
+
+    /**
+     * Gets the animalID
+     *
+     * @return value of animalID
+     */
+    public int getAnimalID() {
+        return animalID;
+    }
+
+    /**
+     * Sets the animalID
+     *
+     * @param animalID the value to set
+     */
+
+    public void setAnimalID(int animalID) {
+        this.animalID = animalID;
+    }
+
+    /**
+     * Gets the name
+     *
+     * @return value of name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the name
+     *
+     * @param name the value to set
+     */
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets the age
+     *
+     * @return value of age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * Sets the age
+     *
+     * @param age the value to set
+     */
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * Gets the gender
+     *
+     * @return value of gender
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * Sets the gender
+     *
+     * @param gender the value to set
+     */
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    /**
+     * Gets the height
+     *
+     * @return value of height
+     */
+    public double getHeight() {
+        return height;
+    }
+
+    /**
+     * Sets the height
+     *
+     * @param height the value to set
+     */
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    /**
+     * Gets the birthday
+     *
+     * @return value of birthday
+     */
+    public String getBirthday() {
+        return birthday;
+    }
+
+    /**
+     * Sets the birthday
+     *
+     * @param birthday the value to set
+     */
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    /**
+     * Gets the food
+     *
+     * @return value of food
+     */
+    public String getFood() {
+        return food;
+    }
+
+    /**
+     * Sets the food
+     *
+     * @param food the value to set
+     */
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+}
---
+++ src/ch/bzz/projekt/model/Zoo.java
@@ -0,0 +1,21 @@
+package ch.bzz.projekt.model;
+
+/**
+ * short description
+ * <p>
+ * Projektarbeit
+ *
+ * @author Nirushaan Sureshkumar
+ * @version 1.0
+ * @since 27.02.20
+ */
+public class Zoo {
+    private String nameZoo;
+    private String adresse;
+    private String plz;
+    private int anzahlTiere;
+    private String mail;
+    private String tel;
+
+
+}
